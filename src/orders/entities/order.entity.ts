11ff2e9506ce92ccff2077cc6e90941d0ac1365c
Luis Alfredo <alf.182.u@gmail.com>
2023-07-02 20:06:07 -0400

    Actualizando

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,5 +1,5 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { Types, Schema as MongooseSchema } from 'mongoose';
+import { Types } from 'mongoose';
 import { now, Document } from 'mongoose';
 import { Detail } from './detail.entity';
 
